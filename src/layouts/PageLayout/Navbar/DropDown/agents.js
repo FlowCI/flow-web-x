f6bed29b84148e771f39749b61cfa602ecaff9b9
cy <cy@fir.im>
2017-08-03 14:11:06 +0800

    添加 agent dropdown menu

--- src/layouts/PageLayout/Navbar/DropDown/agents.js
+++ src/layouts/PageLayout/Navbar/DropDown/agents.js
@@ -1,14 +1,55 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 
+import { connect } from 'react-redux'
+
 import DropDown from './dropdown'
 
 import classes from './agents.scss'
-export default class AgentsDropDown extends PureComponent {
+
+function mapStateToProps (state, props) {
+  return {
+    agents: [{
+      id: '1',
+      status: 'success',
+      name: 'mac - mini',
+      job: 'xiaomi_ios_dev / #2 master',
+    }, {
+      id: '2',
+      status: 'failure',
+      name: 'mac - mini',
+      job: 'xiaomi_ios_dev_xxxxxxxx / #2 master',
+    }]
+  }
+}
+
+export class AgentsDropDown extends PureComponent {
   static propTypes = {
     agents: PropTypes.array,
 
     i18n: PropTypes.func.isRequired,
+  }
+
+  renderAgents () {
+    const { i18n, agents } = this.props
+    return <table className={classes.table}>
+      <thead>
+        <tr>
+          <th className={classes.status}>{i18n('运行状态')}</th>
+          <th className={classes.name}>{i18n('Agent')}</th>
+          <th className={classes.job}>{i18n('任务')}</th>
+        </tr>
+      </thead>
+      <tbody>
+        {agents.map((agent) => <tr key={agent.id}>
+          <td className={classes.statusCell}>
+            <span className={`${classes.circle} ${agent.status}`} />
+          </td>
+          <td>{agent.name}</td>
+          <td>{agent.job}</td>
+        </tr>)}
+      </tbody>
+    </table>
   }
 
   render () {
@@ -17,6 +58,9 @@ export default class AgentsDropDown extends PureComponent {
       <div className={classes.header}>
         {i18n('Agent 状态机')}
       </div>
+      {this.renderAgents()}
     </DropDown>
   }
 }
+
+export default connect(mapStateToProps)(AgentsDropDown)
