f302052b2a08802e456cab9511b411e1618d77a7
cy <cy@fir.im>
2017-08-04 10:10:44 +0800

    fix eslint

--- src/layouts/PageLayout/Navbar/Card/user.js
+++ src/layouts/PageLayout/Navbar/Card/user.js
@@ -1,15 +1,9 @@
 import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
 
 import NavbarUserDropDown from '../DropDown/user'
 import Card from './card'
 
 export default class NavbarUserCard extends PureComponent {
-  static propTypes = {
-    onRequestClose: PropTypes.func,
-    i18n: PropTypes.func.isRequired,
-  }
-
   render () {
     return <Card {...this.props}
       dropDown={<NavbarUserDropDown {...this.props} />}>
