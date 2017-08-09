44e2f19be95d5a32e4342aa5b850fedbc5ce59dd
cy <cy@fir.im>
2017-08-09 11:22:35 +0800

    添加 SignIn 路由

--- src/views/route.js
+++ src/views/route.js
@@ -1,12 +1,16 @@
 import CoreLayout from 'layouts/PageLayout'
 import Home from './Home'
 import Next from './Next'
+import SignInRoute from './SignIn/route'
 
 export const createRoutes = (store) => ({
   path        : '/',
   component   : CoreLayout,
   indexRoute  : { component: Home },
   childRoutes: [{
+    path: '/signin',
+    ...SignInRoute(store),
+  }, {
     path: '/next',
     component: Next
   }]
