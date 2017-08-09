92b5678d3171a9a7f33b2c113e2b92024d0e1459
cy <cy@fir.im>
2017-08-09 11:22:26 +0800

    修改 signIn  action

--- src/redux/modules/session.js
+++ src/redux/modules/session.js
@@ -1,25 +1,21 @@
 import { handleActions } from 'redux-actions'
 import { handleHttp } from '../util'
-import { STATUS } from 'redux-http'
 import Types from './sessionType'
 import { fromJS } from 'immutable'
 
 const initialState = fromJS({
-  user: {
-    id: 'sss',
-    name: 'wcy',
-    email: 'cy@fir.im',
-    avatar: '//flow.ci/logo.svg',
-  },
+  user: undefined,
   ui: {},
 })
 
 export const actions = {
   signIn: function (user) {
     return {
-      type: Types.signIn,
-      status: STATUS.success,
-      payload: {
+      url: '/login',
+      method: 'post',
+      params: user,
+      name: Types.signIn,
+      response: {
         id: '123123123123',
         name: 'wcy',
         email: 'cy@fir.im',
