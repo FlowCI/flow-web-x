f54f7e16e66b3c36ff4abf30c72083b3862e9aa4
cy <cy@fir.im>
2017-08-10 18:46:28 +0800

    修改 完善 flow view

--- src/views/Flows/flow.js
+++ src/views/Flows/flow.js
@@ -1,8 +1,40 @@
-import React, { Component } from 'react'
-// import PropTypes from 'prop-types'
+import { Component } from 'react'
+import PropTypes from 'prop-types'
 
-export default class Flow extends Component {
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import { actions } from 'redux/modules/flow'
+import { actions as jobActions } from 'redux/modules/job'
+
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators({
+    get: actions.get,
+    queryJobs: jobActions.query,
+  }, dispatch)
+}
+
+export class FlowView extends Component {
+  static propTypes = {
+    params: PropTypes.shape({
+      flowId: PropTypes.string.isRequired,
+    }).isRequired,
+    children: PropTypes.node,
+
+    get: PropTypes.func.isRequired,
+    queryJobs: PropTypes.func.isRequired,
+  }
+
+  componentDidMount () {
+    const { get, queryJobs, params: { flowId } } = this.props
+    get(flowId)
+    queryJobs(flowId)
+  }
+
   render () {
-    return <div>hello world</div>
+    const { children } = this.props
+    return children
   }
 }
+
+export default connect(undefined, mapDispatchToProps)(FlowView)
