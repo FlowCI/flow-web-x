51dbd729f27c55fafe623d10bc60c2aa7a180bfc
cy <cy@fir.im>
2017-08-10 18:58:01 +0800

    添加 autoCancel 及 key

--- src/views/Flows/flow.js
+++ src/views/Flows/flow.js
@@ -3,32 +3,46 @@ import PropTypes from 'prop-types'
 
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import autoCancel from 'react-redux-http'
 
 import { actions } from 'redux/modules/flow'
 import { actions as jobActions } from 'redux/modules/job'
+
+function mapStateToProps (state, props) {
+  const { params: { flowId } } = props
+  return {
+    key: flowId,
+    flowId,
+  }
+}
 
 function mapDispatchToProps (dispatch) {
   return bindActionCreators({
     get: actions.get,
     queryJobs: jobActions.query,
+    freed: actions.freed,
   }, dispatch)
 }
 
 export class FlowView extends Component {
   static propTypes = {
-    params: PropTypes.shape({
-      flowId: PropTypes.string.isRequired,
-    }).isRequired,
+    flowId: PropTypes.string.isRequired,
     children: PropTypes.node,
 
     get: PropTypes.func.isRequired,
+    freed: PropTypes.func.isRequired,
     queryJobs: PropTypes.func.isRequired,
   }
 
   componentDidMount () {
-    const { get, queryJobs, params: { flowId } } = this.props
+    const { get, queryJobs, flowId } = this.props
     get(flowId)
     queryJobs(flowId)
+  }
+
+  componentWillUnmount () {
+    const { freed, flowId } = this.props
+    freed(flowId)
   }
 
   render () {
@@ -37,4 +51,6 @@ export class FlowView extends Component {
   }
 }
 
-export default connect(undefined, mapDispatchToProps)(FlowView)
+export default connect(mapStateToProps, mapDispatchToProps)(
+  autoCancel({ funcs: ['queryJobs'] })(FlowView)
+)
