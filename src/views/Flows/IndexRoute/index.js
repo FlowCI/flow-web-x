cef44345f65df7b0c2ec1b9cde78140d1e93c6a7
cy <cy@fir.im>
2017-08-10 18:45:31 +0800

    添加 flows IndexRoute 组件

---
+++ src/views/Flows/IndexRoute/index.js
@@ -0,0 +1,63 @@
+// to show build guide or redirect to jobs
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { push } from 'react-router-redux'
+
+import { STATUS } from 'redux-http'
+
+import Loading from 'components/Loading'
+import Guide from 'views/BuildGuide'
+
+function mapStateToProps (state, props) {
+  const { job } = state
+  const { location } = props
+  const status = job.getIn(['ui', 'query'])
+  return {
+    hasJob: job.get('data').size > 0,
+    loaded: status > STATUS.send,
+    baseHref: location.pathname,
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators({
+    redirect: push,
+  }, dispatch)
+}
+
+export class FlowIndexRoute extends Component {
+  static propTypes = {
+    hasJob: PropTypes.bool,
+    loaded: PropTypes.bool,
+    baseHref: PropTypes.string,
+    redirect: PropTypes.func.isRequired,
+  }
+
+  state = {
+    loaded: this.props.loaded
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (nextProps.hasJob) {
+      const { baseHref, redirect } = nextProps
+      redirect(`${baseHref}/jobs`)
+    } else if (!this.state.loaded && nextProps.loaded) {
+      this.setState({ loaded: true })
+    }
+  }
+
+  render () {
+    const { loaded } = this.state
+    if (loaded) {
+      return <Guide />
+    }
+    return <div>
+      <Loading />
+    </div>
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FlowIndexRoute)
