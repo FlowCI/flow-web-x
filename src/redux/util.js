608ef677e687744dde73ed8f69a34b19de209a4d
cy <cy@fir.im>
2017-08-11 15:55:26 +0800

    删除 无用方法

--- src/redux/util.js
+++ src/redux/util.js
@@ -1,5 +1,4 @@
 import { handleHttpActions } from 'redux-http'
-import { OrderedMap, Map } from 'immutable'
 
 function createUIHandler (key) {
   return function (state, { status, id }) {
@@ -26,32 +25,4 @@ export function handleHttp (uiName, reducers = {}) {
     cancel: combine(createUIHandler(uiName), reducers.cancel),
     failure: combine(createUIHandler(uiName), reducers.failure),
   })
-}
-
-export const defaultInitState = new Map({
-  data: new OrderedMap(),
-  ui: new Map(),
-})
-
-export function mergeArray (state, { payload: array }, option = {}) {
-  const idKey = option.id || 'id'
-  const f = array.reduce((s, item) => {
-    const id = item[idKey]
-    s[id] = item
-    return s
-  }, { })
-  return state.update('data', (data) => data.merge(f))
-}
-
-export function merge (state, { payload: obj }, option = {}) {
-  const idKey = option.id || 'id'
-  const id = obj[idKey]
-
-  // maybe use merge
-  return state.update('data', (data) => data.merge({ [id]: obj }))
-}
-
-export function remove (state, { payload: id }) {
-  return state.update('ids', (ids) => ids.delete(id))
-    .update('data', (data) => data.delete(id))
 }
