6791b639a1cc3ca72dfecbc17df2be15b6e97bdb
cy <cy@fir.im>
2017-08-14 11:23:20 +0800

    添加 job 路由

--- src/views/Flows/Jobs/route.js
+++ src/views/Flows/Jobs/route.js
@@ -2,8 +2,14 @@
 
 import Component from './index'
 
+import JobRoute from './Job/route'
+
 export default function (store) {
   return {
     component: Component,
+    childRoutes: [{
+      ...JobRoute(store),
+      path: ':jobId'
+    }]
   }
 }
