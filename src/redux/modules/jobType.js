ba3937c3960e977697582e1e7fa694e088a0f0c7
cy <cy@fir.im>
2017-08-15 15:38:02 +0800

    修改 getLatest 为 queryLastest

--- src/redux/modules/jobType.js
+++ src/redux/modules/jobType.js
@@ -1,7 +1,7 @@
 export default {
   query: 'JOB_QUERY',
   get: 'JOB_GET',
-  getLatest: 'JOB_GET_FLOWS_LASTEST',
+  queryLastest: 'JOB_QUERY_FLOWS_LASTEST',
   updateFilter: 'JOB_FILTER_UPDATE',
   freedFilter: 'JOB_FILTER_FREED',
   freedAll: 'JOB_FREED_ALL',
