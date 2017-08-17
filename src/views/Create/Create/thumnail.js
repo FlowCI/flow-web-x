d03f7d376c57b13635c88385f1e914fd064d9426
cy <cy@fir.im>
2017-08-17 15:39:31 +0800

    添加 创建 flow 缩略图

--- src/views/Create/Create/thumnail.js
+++ src/views/Create/Create/thumnail.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 
-export default class CreateFlowThumnail extends Component {
+import { connect } from 'react-redux'
+
+import classes from './thumnail.scss'
+
+function mapStateToProps (state, { params: { flowId, git } }) {
+  return {
+    flowId,
+    git,
+  }
+}
+
+export class CreateFlowThumnail extends Component {
   static propTypes = {
-    // active: PropTypes.bool
+    flowId: PropTypes.string.isRequired,
+    // git: PropTypes.string,
   }
 
   render () {
-    return <div>
-      Hello World
+    const { flowId } = this.props
+    return <div className={classes.thumnail}>
+      <span className={classes.icon}>
+        <i className='icon icon-equalizer' />
+      </span>
+      <h4 className={classes.wrapper}>
+        {flowId}
+      </h4>
     </div>
   }
 }
+
+export default connect(mapStateToProps)(CreateFlowThumnail)
