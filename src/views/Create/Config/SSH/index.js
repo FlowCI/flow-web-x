372643f27a233b521565de1407d79a015e3449e5
cy <cy@fir.im>
2017-08-18 16:20:35 +0800

    修改 补全 SSH 配置 逻辑

--- src/views/Create/Config/SSH/index.js
+++ src/views/Create/Config/SSH/index.js
@@ -44,55 +44,122 @@ export class SSHConfig extends Component {
     i18n: PropTypes.func.isRequired,
   }
 
-  render () {
+  state = {
+    url: ''
+  }
+
+  componentWillReceiveProps (nextProps) {
+    const { flow } = nextProps
+    if (flow.getIn(['env', 'FLOW_STATUS']) === 'READY') {
+      const { redirect } = nextProps
+      redirect(`/flows/${flow.get('id')}`)
+    }
+  }
+
+  handleUrlChange = (value) => {
+    this.setState({ url: value })
+  }
+
+  handleDoneCick = () => {
+    const { done, flow } = this.props
+    const { url } = this.state
+    return done(flow.get('id'), {
+      FLOW_STATUS: 'READY',
+      FLOW_GIT_URL: url
+    })
+  }
+
+  handleTestClick = () => {
+    const { test, flow } = this.props
+    const { url } = this.state
+    return test(flow.get('id'), {
+      FLOW_GIT_URL: url
+    })
+  }
+
+  valid (values) {
+    const { url } = values
+    return /^git@\w+\.\w+/.test(url)
+  }
+
+  renderGitUrl () {
+    const { i18n } = this.props
+    const { url } = this.state
+    return <section className={classes.section}>
+      <h5 className={classes.title}>
+        {i18n('输入 Git 仓库地址')}
+        <IconButton className={classes.question}>
+          <i className='icon icon-question-thin' />
+        </IconButton>
+      </h5>
+      <Input className={classes.addr} value={url}
+        type='url' onChange={this.handleUrlChange}
+        placeholder={i18n('例：git@github.com:FlowCI/flow-platform.git')} />
+    </section>
+  }
+
+  renderWebhook () {
     const { i18n, flow } = this.props
     const webhook = flow.getIn(['env', 'FLOW_GIT_WEBHOOK'])
+    return <section className={classes.section}>
+      <h5 className={classes.title}>
+        {i18n('手动添加 WebHook 地址到你的 Git 仓库')}
+        <IconButton className={classes.question}>
+          <i className='icon icon-question-thin' />
+        </IconButton>
+      </h5>
+      <code className={classes.code}>
+        {webhook}
+        {supportCopy && <ClipboardButton
+          className={`btn btn-link ${classes.copy}`}
+          data-clipboard-text={webhook}>
+          {i18n('复制地址')}
+        </ClipboardButton>}
+      </code>
+    </section>
+  }
+
+  renderDeploy () {
+    const { i18n } = this.props
+    return <section className={classes.section}>
+      <h5 className={classes.title}>
+        {i18n('Deploy Key（可选）')}
+        <IconButton className={classes.question}>
+          <i className='icon icon-question-thin' />
+        </IconButton>
+        <small className={classes.subTitle}>
+          {i18n('如没有 Git 仓库访问权限，请添加 Deploy Key 到 Git 仓库的项目或者用户设置')}
+        </small>
+      </h5>
+    </section>
+  }
+
+  renderActions () {
+    const { i18n } = this.props
+    const enabled = this.valid(this.state)
+
+    return <div className={classes.actions}>
+      <Button className='btn-primary'
+        disabled={!enabled}
+        onClick={this.handleDoneCick}
+      >
+        {i18n('完成')}
+      </Button>
+      <Button className='btn-inverse'
+        disabled={!enabled}
+        onClick={this.handleTestClick}
+      >
+        {i18n('连接测试')}
+      </Button>
+    </div>
+  }
+
+  render () {
     return <div>
-      <section className={classes.section}>
-        <h5 className={classes.title}>
-          {i18n('输入 Git 仓库地址')}
-          <IconButton className={classes.question}>
-            <i className='icon icon-question-thin' />
-          </IconButton>
-        </h5>
-        <Input className={classes.addr}
-          placeholder={i18n('例：git@github.com:FlowCI/flow-platform.git')} />
-      </section>
-      <section className={classes.section}>
-        <h5 className={classes.title}>
-          {i18n('手动添加 WebHook 地址到你的 Git 仓库')}
-          <IconButton className={classes.question}>
-            <i className='icon icon-question-thin' />
-          </IconButton>
-        </h5>
-        <code className={classes.code}>
-          {webhook}
-          {supportCopy && <ClipboardButton
-            className={`btn btn-link ${classes.copy}`}
-            data-clipboard-text={webhook}>
-            {i18n('复制地址')}
-          </ClipboardButton>}
-        </code>
-      </section>
-      <section className={classes.section}>
-        <h5 className={classes.title}>
-          {i18n('Deploy Key（可选）')}
-          <IconButton className={classes.question}>
-            <i className='icon icon-question-thin' />
-          </IconButton>
-          <small className={classes.subTitle}>
-            {i18n('如没有 Git 仓库访问权限，请添加 Deploy Key 到 Git 仓库的项目或者用户设置')}
-          </small>
-        </h5>
-      </section>
-      <div className={classes.actions}>
-        <Button className='btn-primary'>
-          {i18n('完成')}
-        </Button>
-        <Button className='btn-inverse' loading>
-          {i18n('连接测试')}
-        </Button>
-      </div>
+      {this.renderGitUrl()}
+      {this.renderWebhook()}
+      {this.renderDeploy()}
+      {this.renderActions()}
     </div>
   }
 }
