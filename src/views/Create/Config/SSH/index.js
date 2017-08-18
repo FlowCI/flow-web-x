a4e8a570c2fbbf36e20c4515d402d192f7d3376b
cy <cy@fir.im>
2017-08-18 18:27:44 +0800

    提取 webhook section 至组件中

--- src/views/Create/Config/SSH/index.js
+++ src/views/Create/Config/SSH/index.js
@@ -2,24 +2,20 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import ImmutablePropTypes from 'react-immutable-proptypes'
 
-import clipboard from 'clipboard'
-
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { push } from 'react-router-redux'
 
 import { actions } from 'redux/modules/flow'
 
-import ClipboardButton from 'react-clipboard.js'
-
 import Input from 'components/Form/Input'
 import Button from 'components/Button'
 
 import { Section, SectionTitle } from '../components/Section'
+import WebhookSection from '../components/WebhookSection'
 
 import classes from './ssh.scss'
 
-const supportCopy = clipboard.isSupported()
 function mapStateToProps (state, { flowId }) {
   const { flow } = state
   return {
@@ -47,7 +43,6 @@ export class SSHConfig extends Component {
 
   state = {
     url: '',
-    copied: false
   }
 
   componentWillReceiveProps (nextProps) {
@@ -58,19 +53,8 @@ export class SSHConfig extends Component {
     }
   }
 
-  componentWillUnmount () {
-    clearTimeout(this.copyTimer)
-  }
-
   handleUrlChange = (value) => {
     this.setState({ url: value })
-  }
-
-  handleCopySuccess = () => {
-    this.setState({ copied: true })
-    this.copyTimer = setTimeout(() => {
-      this.setState({ copied: false })
-    }, 3000)
   }
 
   handleDoneCick = () => {
@@ -110,26 +94,8 @@ export class SSHConfig extends Component {
 
   renderWebhook () {
     const { i18n, flow } = this.props
-    const { copied } = this.state
     const webhook = flow.getIn(['env', 'FLOW_GIT_WEBHOOK'])
-    return <Section>
-      <SectionTitle title={i18n('手动添加 WebHook 地址到你的 Git 仓库')}
-        question='link for doc'
-      />
-      <code className={classes.code}>
-        {webhook}
-        {supportCopy && <ClipboardButton
-          className={`btn btn-link ${classes.copy}`}
-          data-clipboard-text={webhook}
-          onSuccess={this.handleCopySuccess}
-        >
-          <span>
-            {i18n('复制地址')}
-            {copied && <i className='icon icon-check text-success' />}
-          </span>
-        </ClipboardButton>}
-      </code>
-    </Section>
+    return <WebhookSection i18n={i18n} webhook={webhook} />
   }
 
   renderDeploy () {
