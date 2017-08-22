ece849bc0a0bc85537e3278e9948a2312824072c
cy <cy@fir.im>
2017-08-22 17:40:05 +0800

    修正 空 ignore 时 过滤错误

--- src/mock/bundle.js
+++ src/mock/bundle.js
@@ -1,8 +1,9 @@
-const ignore = ['flow']
+const ignore = []
 const ignoreReg = new RegExp(`${ignore.join('|')}.js`)
 
 const context = require.context('./database', false, /\.js$/)
-const keys = context.keys().filter((key) => !ignoreReg.test(key))
+let keys = context.keys()
+keys = ignore.length ? keys.filter((key) => !ignoreReg.test(key)) : keys
 
 const datas = keys.map((key) => context(key).default)
 
