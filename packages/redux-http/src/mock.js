2345d9219015c0a40582d6c83cd41487e71a5abc
cy <cy@fir.im>
2017-08-22 16:57:27 +0800

    添加 all 配置，用于决定未找到 handler 时是提示还是走 next

--- packages/redux-http/src/mock.js
+++ packages/redux-http/src/mock.js
@@ -13,44 +13,42 @@ function isFunction (v) {
 
 export default function (config = {}) {
   const HANDLE_TYPE = config.type
-  const database = config.database
-
+  const { database, all = false } = config.database
   return function ({ dispatch, getState }) {
     return (next) => (action) => {
       const { name, url, type } = action
       if (!name || !url || (HANDLE_TYPE && type !== HANDLE_TYPE)) {
         return next(action)
       }
-
+      const responseHandle = database(name)
+      if (!responseHandle) {
+        return all ? console.error('unfound response', action) : next(action)
+      }
       const {
         indicator, delay,
         transformResponse
       } = action
 
-      const responseHandle = database(name)
-      if (responseHandle) {
-        let response = responseHandle(action)
-        if (transformResponse) {
-          if (isFunction(transformResponse)) {
-            response = transformResponse(response)
-          } else {
-            response = transformResponse.reduce((data, f) => f(data), response)
-          }
+      let response = responseHandle(action)
+      if (transformResponse) {
+        if (isFunction(transformResponse)) {
+          response = transformResponse(response)
+        } else {
+          response = transformResponse.reduce((data, f) => f(data), response)
         }
-
-        const promise = makeCancelable(new Promise((resolve, reject) => {
-          setTimeout(() => {
-            resolve({ status: 200, data: response })
-          }, delay || 100)
-        }))
-
-        const indicatorAction = { type: name, indicator }
-        const result = makeIndicator(dispatch, promise, indicatorAction)
-
-        copyCancel(result, promise)
-        return result
       }
-      console.error('unfound response', action)
+
+      const promise = makeCancelable(new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve({ status: 200, data: response })
+        }, delay || 100)
+      }))
+
+      const indicatorAction = { type: name, indicator }
+      const result = makeIndicator(dispatch, promise, indicatorAction)
+
+      copyCancel(result, promise)
+      return result
     }
   }
 }
