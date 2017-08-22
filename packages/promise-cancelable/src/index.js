34ce6025244b220b5ba1670af41e1c12e6f3cea2
cy <cy@fir.im>
2017-08-22 16:49:13 +0800

    添加 promise-cancelable package

---
+++ packages/promise-cancelable/src/index.js
@@ -0,0 +1,16 @@
+import makeCancelable, {
+  getCancel,
+  cloneCancel,
+  isPromise,
+  cancel
+} from './makeCancelable'
+import isCancel from './isCancel'
+
+export default makeCancelable
+export {
+  getCancel,
+  cloneCancel,
+  isPromise,
+  cancel,
+  isCancel,
+}
---
+++ packages/promise-cancelable/tests/promise.spec.js
@@ -0,0 +1,47 @@
+import makeCancelable, {
+  cloneCancel,
+  cancel,
+  isCancel,
+} from 'index'
+
+describe('Make Promsie Cancelable', function () {
+  it ('should reject an cancel object when promise cancel', () => {
+    const cancelSpy = sinon.spy(cancel)
+
+    const promise = new Promise (function (resolve, reject) {
+      setTimeout(resolve, 2000)
+    })
+    const promiseCancelable = makeCancelable(promise)
+    setTimeout(function () {
+      cancelSpy(promiseCancelable)
+    })
+
+    return promiseCancelable.then(() => {
+      expect(false).to.be.true // the promise must not resolve
+    }, (e) => {
+      expect(isCancel(e)).to.be.true
+    }).then(() => {
+      cancelSpy.should.have.been.calledOnce
+    })
+  })
+
+  describe('function copyCancel', function () {
+    it ('target can cancel source promsie', function () {
+      const p1 = makeCancelable(new Promise(function (resolve, reject) {
+        setTimeout(resolve, 2000)
+      }))
+      const p2 = p1.then(() => {
+        expect(false).to.be.true // the p1 must be rejected
+      })
+
+      cloneCancel(p2, p1)
+      cancel(p2) // it will cancel p1
+
+      return p2.then(() => {
+        expect(false).to.be.true // the p1 must be rejected
+      }).catch((e) => {
+        expect(isCancel(e)).to.be.true
+      })
+    })
+  })
+})
