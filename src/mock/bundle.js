b816f02a4976385d3fe868db256958557d06bf7f
cy <cy@fir.im>
2017-08-22 17:44:48 +0800

    fix 使用 const 替代 let

--- src/mock/bundle.js
+++ src/mock/bundle.js
@@ -2,7 +2,7 @@ const ignoreFiles = []
 const ignoreReg = new RegExp(`^${ignoreFiles.join('|')}.js$`)
 
 const context = require.context('./database', false, /\.js$/)
-let keys = context.keys().filter((key) => !ignoreReg.test(key))
+const keys = context.keys().filter((key) => !ignoreReg.test(key))
 
 const datas = keys.map((key) => context(key).default)
 
