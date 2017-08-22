5ea3f46f34a951a9cecc720f81667ea234f3a54e
cy <cy@fir.im>
2017-08-22 16:51:20 +0800

    添加 ignore 配置

--- src/mock/bundle.js
+++ src/mock/bundle.js
@@ -1,5 +1,8 @@
+const ignore = ['flow']
+const ignoreReg = new RegExp(`${ignore.join('|')}.js`)
+
 const context = require.context('./database', false, /\.js$/)
-const keys = context.keys()
+const keys = context.keys().filter((key) => !ignoreReg.test(key))
 
 const datas = keys.map((key) => context(key).default)
 
