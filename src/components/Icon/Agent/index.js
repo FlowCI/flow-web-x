f740844ad5b09d3bafee87f8a01a1026d7059ebe
cy <cy@fir.im>
2017-08-28 16:59:55 +0800

    添加 icon 颜色类

--- src/components/Icon/Agent/index.js
+++ src/components/Icon/Agent/index.js
@@ -11,19 +11,25 @@ export default class AgentIcon extends Component {
   render () {
     const { status } = this.props
     let content = <i className={classes.line} />
+    let colorClass = ''
     switch (status) {
       case 'OFFLINE':
         content = <i className='icon icon-off' />
+        colorClass = classes.black
         break
       case 'RUNNING':
+        colorClass = classes.green
         content = <span className={classes.dots}>
           <i />
           <i />
           <i />
         </span>
         break
+      case 'IDLE':
+        colorClass = classes.yellow
     }
-    const cls = [classes.icon, classes[status]]
+    const cls = [classes.icon, status]
+    colorClass && cls.push(colorClass)
     return <span className={cls.join(' ')}>
       {content}
     </span>
