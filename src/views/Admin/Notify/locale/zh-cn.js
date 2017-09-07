c1d5435fabe5c8953a0166a1c7f5043abc73884e
cy <cy@fir.im>
2017-09-07 16:28:58 +0800

    修改 placeholder 描述

--- src/views/Admin/Notify/locale/zh-cn.js
+++ src/views/Admin/Notify/locale/zh-cn.js
@@ -3,16 +3,16 @@ export default {
   subTitle: '用于发送邮件，目前只提供 SMTP 模式',
 
   smtpUrl: 'SMTP 服务器',
-  smtpUrlPlaceholder: 'SMTP 服务器地址',
+  smtpUrlPlaceholder: 'smtp.example.com',
 
   smtpPort: 'SMTP 端口',
-  smtpPortPlaceholder: 'SMTP 端口',
+  smtpPortPlaceholder: '',
 
   sender: '发信人邮箱',
-  senderPlaceholder: '',
+  senderPlaceholder: 'email@example.com',
 
   username: '验证用户名',
-  usernamePlaceholder: 'SMTP 用户名',
+  usernamePlaceholder: '',
 
   password: '验证密码',
   passwordPlaceholder: '',
