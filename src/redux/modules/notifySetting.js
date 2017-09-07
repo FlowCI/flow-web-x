1a6266086c031662d7a64868cc038698df8c15e4
cy <cy@fir.im>
2017-09-07 15:25:57 +0800

    添加 notifySetting reduce

---
+++ src/redux/modules/notifySetting.js
@@ -0,0 +1,46 @@
+import { handleActions } from 'redux-actions'
+import { Map, fromJS } from 'immutable'
+
+import { handleHttpActions } from 'redux-http'
+
+import types from './notifySettingType'
+
+const initialState = fromJS({ ui: {} })
+
+export const actions = {
+  getEmailSetting: function () {
+    return {
+      url: '/message/email/settings',
+      name: types.getEmail,
+    }
+  },
+  testEmailSetting: function (setting) {
+    return {
+      url: '/message/email/settings/auth',
+      name: types.testEmail,
+      method: 'post',
+      params: setting,
+    }
+  },
+  saveEmailSetting: function (setting) {
+    return {
+      url: '/message/email/settings',
+      name: types.saveEmail,
+      method: 'post',
+      params: setting,
+    }
+  }
+}
+
+export default handleActions({
+  [types.getEmail]: handleHttpActions('GET_EMAIL', {
+    success: function (state, { payload }) {
+      return state.set('email', fromJS(payload))
+    }
+  }),
+  [types.saveEmail]: handleHttpActions('SAVE_EMAIL', {
+    success: function (state, { payload }) {
+      return state.set('email', fromJS(payload))
+    }
+  })
+}, initialState)
