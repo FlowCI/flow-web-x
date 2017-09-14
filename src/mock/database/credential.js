9ea010e4d8ec03b87f4061bc50137d124e46bdf8
cy <cy@fir.im>
2017-09-14 18:18:53 +0800

    修改 query crednetial 返回数据，与后端统一

--- src/mock/database/credential.js
+++ src/mock/database/credential.js
@@ -2,29 +2,60 @@ import types from 'redux/modules/credentialType'
 
 export default {
   [types.query]: function () {
-    return {
-      ios: [{
-        name: '证书1',
-        author: 'gy',
-        createdAt: 1502266861,
-
-      }],
-      rsa: [{
-        name: 'c-name',
-        author: 'gy',
-        createdAt: 1502266861,
-        fingerprint: '92:27:9d:77:fc:17:58:31:e7:32:03:bf:35:f7:ef:8a',
-      }, {
-        name: 'c-name2',
-        author: 'gy',
-        createdAt: 1502266861,
-        fingerprint: '92:27:9d:77:fc:17:58:31:e7:32:03:bf:35:f7:ef:8a',
-      }, {
-        name: 'c-name3',
-        author: 'gy',
-        createdAt: 1502266861,
-        fingerprint: '92:27:9d:77:fc:17:58:31:e7:32:03:bf:35:f7:ef:8a',
-      }]
-    }
+    return [
+      {
+        'name': 'android-credential',
+        'type': 'ANDROID',
+        'detail': {
+          'file': {
+            'name': 'android.jks'
+          },
+          'keyStorePassword': '12345',
+          'keyStoreAlias': 'android',
+          'keyStoreAliasPassword': '12345'
+        },
+        'createdAt': 1504737923,
+        'updatedAt': 1504737923
+      },
+      {
+        'name': 'ios-credential',
+        'type': 'IOS',
+        'detail': {
+          'provisionProfiles': [
+            {
+              'name': 'pp'
+            }
+          ],
+          'p12s': [
+            {
+              'password': '12345',
+              'name': 'p12'
+            }
+          ]
+        },
+        'createdAt': 1504737923,
+        'updatedAt': 1504737923
+      },
+      {
+        'name': 'ras-credential',
+        'type': 'RSA',
+        'detail': {
+          'publicKey': 'public key',
+          'privateKey': 'private key'
+        },
+        'createdAt': 1504737923,
+        'updatedAt': 1504737923
+      },
+      {
+        'name': 'username-credential',
+        'type': 'USERNAME',
+        'detail': {
+          'username': 'user',
+          'password': 'pass'
+        },
+        'createdAt': 1504737923,
+        'updatedAt': 1504737923
+      }
+    ]
   },
 }
