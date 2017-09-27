3fa08c1634b86d1dd5795f0e38fc321df6c5b91e
cy <cy@fir.im>
2017-09-27 16:20:01 +0800

    修改 deploylistitem 布局方式

--- src/views/Create/Config/SSH/deployItem.js
+++ src/views/Create/Config/SSH/deployItem.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import ImmutablePropTypes from 'react-immutable-proptypes'
 
-import Checkbox from 'components/Form/Checkbox'
+import Checkbox, { classes as checkboxClass } from 'components/Form/Checkbox'
 import Button from 'components/Button'
 import ClipboardButton from 'components/ClipboardButton'
 
@@ -11,6 +11,8 @@ import classes from './deploy.scss'
 const checkedIcon = <i className='icon checked icon-circle-check' />
 const unCheckedIcon = <i className='icon checked icon-radio-unchecked' />
 
+const checkboxClassNames = { ...checkboxClass, label: classes.label }
+
 export default class DeployListItem extends Component {
   static propTypes = {
     deploy: ImmutablePropTypes.map.isRequired,
@@ -18,32 +20,41 @@ export default class DeployListItem extends Component {
     onChange: PropTypes.func.isRequired,
   }
 
+  state = {
+    showAll: false,
+  }
+
   handleChange = (v) => {
     const { onChange, deploy } = this.props
     return onChange(deploy, v)
   }
 
-  render () {
-    const { checked, deploy } = this.props
-    const deployKey = deploy.getIn(['detail', 'publickKey'])
+  renderThumail () {
+    const { deploy } = this.props
+    const deployKey = deploy.getIn(['detail', 'publicKey'])
+    return <div className={classes.thumail}>
+      <span>{deploy.get('name')}</span>
+      <ul className={classes.actions}>
+        <li>
+          <Button className='btn-link' size='sm'>显示完整内容</Button>
+        </li>
+        <li>
+          <ClipboardButton className='btn-link' size='sm'
+            data-clipboard-text={deployKey} />
+        </li>
+      </ul>
+    </div>
+  }
 
-    return <li className={classes.item}>
-      <Checkbox
+  render () {
+    const { checked } = this.props
+    return <li>
+      <Checkbox classNames={checkboxClassNames}
+        className={classes.item}
+        rightLabel={this.renderThumail()}
         checked={checked} onChange={this.handleChange}
         checkedIcon={checkedIcon} unCheckedIcon={unCheckedIcon}
       />
-      <div className={classes.desc}>
-        <span>{deploy.get('name')}</span>
-        <ul className={classes.actions}>
-          <li>
-            <Button className='btn-link' size='sm'>显示完整内容</Button>
-          </li>
-          <li>
-            <ClipboardButton className='btn-link' size='sm'
-              data-clipboard-text={deployKey} />
-          </li>
-        </ul>
-      </div>
     </li>
   }
 }
