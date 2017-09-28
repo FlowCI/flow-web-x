54c2a504d6e005dcf0345444f72203eab14e912f
Yang Guo <gy@fir.im>
2017-09-28 11:53:32 +0800

    add job category for i18n zh

--- src/views/Flows/Jobs/Job/locale/zh-cn.js
+++ src/views/Flows/Jobs/Job/locale/zh-cn.js
@@ -1,4 +1,12 @@
 import moment from 'moment'
+
+const JobCategory = {
+  MANUAL: '手动构建',
+  PUSH: 'Push',
+  PR: 'Pull Request',
+  TAG: 'Tag'
+}
+
 export default {
   buildDuration: function ({ duration }) {
     return `花费 ${duration} 秒`
@@ -6,5 +14,8 @@ export default {
   buildFromNow: function ({ time }) {
     const t = moment(time * 1000).fromNow()
     return `构建于 ${t}`
+  },
+  jobCategory: function ({ category }) {
+    return JobCategory[category] || '-'
   }
 }
