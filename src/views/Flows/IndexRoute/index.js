d159457a16894e1d58d4421e14883614a3295467
cy <cy@fir.im>
2017-09-29 12:11:30 +0800

    修改 index 路由跳转地址判断

--- src/views/Flows/IndexRoute/index.js
+++ src/views/Flows/IndexRoute/index.js
@@ -1,24 +1,18 @@
 // to show build guide or redirect to jobs
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import ImmutablePropTypes from 'react-immutable-proptypes'
 
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { replace } from 'react-router-redux'
 
-import { STATUS } from 'redux-http'
-
-import Loading from 'components/Loading'
-import Guide from 'views/BuildGuide'
-
 function mapStateToProps (state, props) {
-  const { job } = state
-  const { location } = props
-  const status = job.getIn(['ui', 'QUERY'])
+  const { flow } = state
+  const { params: { flowId } } = props
   return {
-    hasJob: job.get('data').size > 0,
-    loaded: status > STATUS.send,
-    baseHref: location.pathname,
+    flowId,
+    flow: flow.getIn(['data', flowId]),
   }
 }
 
@@ -30,40 +24,24 @@ function mapDispatchToProps (dispatch) {
 
 export class FlowIndexRoute extends Component {
   static propTypes = {
-    hasJob: PropTypes.bool,
-    loaded: PropTypes.bool,
-    baseHref: PropTypes.string,
+    flowId: PropTypes.string.isRequired,
+    flow: ImmutablePropTypes.map.isRequired,
     redirect: PropTypes.func.isRequired,
   }
 
-  state = {
-    loaded: this.props.loaded
-  }
-
   componentDidMount () {
-    if (this.props.hasJob) {
-      const { baseHref, redirect } = this.props
-      redirect(`${baseHref}/jobs`)
-    }
-  }
+    const { flow, flowId, redirect } = this.props
+    const status = flow.getIn(['envs', 'FLOW_STATUS'])
 
-  componentWillReceiveProps (nextProps) {
-    if (nextProps.hasJob) {
-      const { baseHref, redirect } = nextProps
-      redirect(`${baseHref}/jobs`)
-    } else if (!this.state.loaded && nextProps.loaded) {
-      this.setState({ loaded: true })
+    if (status === 'READY') {
+      redirect(`/flows/${flowId}/jobs`)
+    } else {
+      redirect(`/create/${flowId}`)
     }
   }
 
   render () {
-    const { loaded } = this.state
-    if (loaded) {
-      return <Guide />
-    }
-    return <div>
-      <Loading />
-    </div>
+    return <div />
   }
 }
 
