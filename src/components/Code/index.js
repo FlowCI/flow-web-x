dd69bf4304fbf98f7179354f3984e4aa747ec54d
cy <cy@fir.im>
2017-10-11 16:20:58 +0800

    添加 code 组件

---
+++ src/components/Code/index.js
@@ -0,0 +1,41 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+import classes from './code.scss'
+
+export default class Code extends PureComponent {
+  static propTypes = {
+    code: PropTypes.string.isRequired,
+    className: PropTypes.string,
+    lineNumbers: PropTypes.bool,
+  }
+
+  static defaultProps = {
+    code: '',
+    className: '',
+    lineNumbers: false,
+  }
+
+  state = {
+    codes: this.props.code.split('\n')
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (this.props.code !== nextProps.code) {
+      this.setState({ codes: nextProps.code.split('\n') })
+    }
+  }
+
+  render () {
+    const { className } = this.props
+    const { codes } = this.state
+    return <table className={`code ${classes.table} ${className}`}>
+      <tbody>
+        {codes.map((code, i) => <tr key={i}>
+          <td className={classes.number}>{i + 1}</td>
+          <td>{code}</td>
+        </tr>)}
+      </tbody>
+    </table>
+  }
+}
