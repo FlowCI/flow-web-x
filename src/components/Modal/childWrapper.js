30b53250a244f65d6546fbd52827720aaf191930
cy <cy@fir.im>
2017-10-13 15:08:13 +0800

    修正 Modal 组件 children 组件生命周期不符合预期

---
+++ src/components/Modal/childWrapper.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class ModalChildWrapper extends Component {
+  static propTypes = {
+    isOpen: PropTypes.bool,
+    children: PropTypes.node,
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    /**
+     * 关闭的时候不进行重新 render, 保证关闭动画时的窗口内容不变
+     */
+    const { isOpen } = nextProps
+    return isOpen
+  }
+
+  render () {
+    const { children } = this.props
+    return <div>
+      {children}
+    </div>
+  }
+}
--- src/components/Modal/wrapper.js
+++ src/components/Modal/wrapper.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import ReactModal from 'react-modal'
 
 import AutoLockScrolling from 'components/AutoLockScrolling'
+import ModalChildWrapper from './childWrapper'
 
 import classes from './wrapper.scss'
 
@@ -29,6 +30,7 @@ export {
   defaultClassNames as classes
 }
 
+const closeTimeoutMS = 300
 export default class ReactModalWrapper extends Component {
   /**
    * more props @see https://reactcommunity.org/react-modal/
@@ -44,10 +46,51 @@ export default class ReactModalWrapper extends Component {
 
     title: PropTypes.string.isRequired,
     children: PropTypes.node,
+
+    onRequestClose: PropTypes.func,
   }
 
   static defaultProps = {
     classNames: defaultClassNames,
+  }
+
+  state = {
+    /**
+     * 保证 children 组件生命周期同视觉
+     */
+    closed: !this.props.isOpen
+  }
+
+  componentDidMount () {
+    this.isMount = true
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (this.props.isOpen !== nextProps.isOpen) {
+      nextProps.isOpen ? this.openModal() : this.closeModal()
+    }
+  }
+
+  componentWillUnmount () {
+    this.isMount = false
+  }
+
+  safeSetClosed (closed) {
+    if (this.isMount) {
+      this.setState({ closed })
+    }
+  }
+
+  openModal = () => {
+    clearTimeout(this.closeTimer)
+    this.safeSetClosed(false)
+  }
+
+  closeModal = () => {
+    clearTimeout(this.closeTimer)
+    this.closeTimer = setTimeout(() => {
+      this.safeSetClosed(true)
+    }, closeTimeoutMS)
   }
 
   render () {
@@ -59,11 +102,12 @@ export default class ReactModalWrapper extends Component {
       classNames,
       ...other,
     } = this.props
+    const { closed } = this.state
     return (
       <ReactModal
         {...other}
         isOpen={isOpen}
-        closeTimeoutMS={300}
+        closeTimeoutMS={closeTimeoutMS}
         contentLabel={title}
         portalClassName={classNames.portal}
         bodyOpenClassName={classNames.bodyOpen}
@@ -72,7 +116,9 @@ export default class ReactModalWrapper extends Component {
         shouldCloseOnOverlayClick={!modal}
       >
         <AutoLockScrolling lock={isOpen} />
-        {children}
+        {!closed && <ModalChildWrapper isOpen={isOpen}>
+          {children}
+        </ModalChildWrapper>}
       </ReactModal>
     )
   }
