4395cb14c8815b4c8e1edb706c5c633a7e214224
cy <cy@fir.im>
2017-10-17 10:42:28 +0800

    添加 创建成功跳转至列表页面

--- src/views/Admin/Agents/Create/create.js
+++ src/views/Admin/Agents/Create/create.js
@@ -4,25 +4,30 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
+import { push } from 'react-router-redux'
 import { actions } from 'redux/modules/agent'
 
 import Form from './form'
 
 function mapDispatchToProps (dispatch) {
   return bindActionCreators({
-    create: actions.create
+    create: actions.create,
+    redirect: push,
   }, dispatch)
 }
 
 export class CreateAgent extends Component {
   static propTypes = {
     create: PropTypes.func.isRequired,
+    redirect: PropTypes.func.isRequired,
   }
 
   handleSbumit = (values) => {
-    const { create } = this.props
+    const { create, redirect } = this.props
     const { zone, name } = values
-    return create(zone, name)
+    return create(zone, name).then(() => {
+      redirect('/admin/agents')
+    })
   }
 
   render () {
