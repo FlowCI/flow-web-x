7538652f9fed166ff3c7523724804b1803fd7455
cy <cy@fir.im>
2017-10-19 17:50:39 +0800

    修改 默认 unit 判断条件

--- src/util/capacity.js
+++ src/util/capacity.js
@@ -3,7 +3,10 @@ const units = ['B', 'KB', 'MB', 'GB']
 export default function (size, defaultUnit) {
   let number = size
   let next
-  let unit = units.indexOf(defaultUnit) || 0
+  let unit = units.indexOf(defaultUnit)
+  if (!(unit >= 0)) {
+    unit = 0
+  }
   const max = units.length - 1
   while ((next = number / radix) > 1 && unit < max) {
     number = next
