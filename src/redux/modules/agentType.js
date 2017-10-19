87f03103f047b7be9841f7066455d6d19687f3ae
cy <cy@fir.im>
2017-10-19 14:36:16 +0800

    删除 无用 agent type

--- src/redux/modules/agentType.js
+++ src/redux/modules/agentType.js
@@ -3,6 +3,4 @@ export default {
   query: 'AGENT_QUERY',
   remove: 'AGENT_REMOVE',
   freedAll: 'AGENT_FREED_ALL',
-  getSystemInfo: 'GET_AGENT_SYSTEM_INFO',
-  storeSystemInfo: 'STORE_AGENT_SYSTEM_INFO'
 }
