b78e5ce0a2badd723c1b06f01877cb964681b30f
cy <cy@fir.im>
2017-10-20 16:51:39 +0800

    修正 历史日志行号获取错误

--- src/redux/modules/node.js
+++ src/redux/modules/node.js
@@ -118,10 +118,10 @@ export default handleActions({
     const { number, content } = log
     return state.updateIn([jobId, 'log', id], (old) => {
       if (old) {
-        if (old.line < number) {
+        if (old.get('line') < number) {
           return new Map({
             line: number,
-            str: [old.str, old.content].join('\n'),
+            str: [old.get('str'), old.content].join('\n'),
           })
         }
       } else {
