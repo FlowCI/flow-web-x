2e5bb71ce5be3d650978075c40630dd381c15a7e
cy <cy@fir.im>
2017-11-02 18:43:48 +0800

    修正 defaultValue 属性导致从 uncontrol 变至 control bug

--- src/components/CodeEditor/index.js
+++ src/components/CodeEditor/index.js
@@ -7,12 +7,37 @@ import classes from './editor.scss'
 
 export default class CodeEditor extends Component {
   static propTypes = {
+    defaultValue: PropTypes.string,
     className: PropTypes.string,
   }
 
+  static defaultProps = {
+    defaultValue: ''
+  }
+
+  state = {
+    value: this.props.defaultValue,
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (this.props.defaultValue !== nextProps.defaultValue) {
+      this.setState({ value: nextProps.defaultValue })
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({ value: e.target.value })
+  }
+
   render () {
-    const { className } = this.props
-    return <textarea {...this.props}
+    const {
+      className,
+      defaultValue,
+      ...other
+    } = this.props
+    const { value } = this.state
+    return <textarea {...other} value={value}
+      onChange={this.handleChange}
       className={classnames(classes.editor, className)} />
   }
 }
