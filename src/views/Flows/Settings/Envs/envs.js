818d7e144edbc6c14e61eeca08e39a6242165c1d
cy <cy@fir.im>
2017-11-06 15:06:04 +0800

    添加 env 展示及编辑删除

--- src/views/Flows/Settings/Envs/envs.js
+++ src/views/Flows/Settings/Envs/envs.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import ImmutablePropTypes from 'react-immutable-proptypes'
 
-import { Map } from 'immutable'
+import { OrderedMap } from 'immutable'
 
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+
+import { actions } from 'redux/modules/flow'
 
 import Header from '../components/Header'
 
@@ -15,24 +17,47 @@ import classes from './envs.scss'
 
 function mapStateToProps (state, props) {
   const { params: { flowId } } = props
+  const { flow } = state
   return {
-    envs: new Map({ key: 'value1', key2: 'value2' }),
+    envs: flow.getIn(['editEnvs', flowId], new OrderedMap()),
     flowId,
   }
 }
 
 function mapDispatchToProps (dispatch) {
   return bindActionCreators({
-    update: function () {}
+    query: actions.getEditEnvs,
+    save: actions.saveEditEnvs,
+    remove: actions.removeEditEnvs,
   }, dispatch)
 }
 
 export class FlowEnvsSetting extends Component {
   static propTypes = {
-    // flowId: PropTypes.string.isRequired,
+    flowId: PropTypes.string.isRequired,
     envs: ImmutablePropTypes.map.isRequired,
-    // update: PropTypes.func.isRequired,
+    query: PropTypes.func.isRequired,
+    save: PropTypes.func.isRequired,
+    remove: PropTypes.func.isRequired,
+  }
 
+  componentDidMount () {
+    const { query, flowId } = this.props
+    query(flowId)
+  }
+
+  handleSave = (name, value) => {
+    const { save, flowId } = this.props
+    return save(flowId, { [name]: value })
+  }
+
+  handleRemove = (name) => {
+    const { remove, flowId } = this.props
+    return remove(flowId, name)
+  }
+
+  handleSubmit = ({ name, value }) => {
+    return this.handleSave(name, value)
   }
 
   render () {
@@ -40,13 +65,15 @@ export class FlowEnvsSetting extends Component {
     return <div>
       <Header title='全局环境变量'
         subTitle='环境变量作用域为整个 flow 流程   查看帮助文档'>
-        <Form className={classes.form} />
+        <Form className={classes.form} onSubmit={this.handleSubmit} />
       </Header>
       <table className={classes.table}>
         <tbody>
           {envs.keySeq().map((key) => {
             const value = envs.get(key)
-            return <Env key={key} name={key} value={value} />
+            return <Env key={key} name={key} value={value}
+              save={this.handleSave} remove={this.handleRemove}
+            />
           })}
         </tbody>
       </table>
