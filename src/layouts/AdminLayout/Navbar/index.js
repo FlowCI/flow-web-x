1fa5ab0e29371c548be7ea7867b1f8a3c3f6cc0d
cy <cy@fir.im>
2017-11-07 17:24:33 +0800

    修改 管理员名称显示错误

--- src/layouts/AdminLayout/Navbar/index.js
+++ src/layouts/AdminLayout/Navbar/index.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
+import { connect } from 'react-redux'
 import { Link } from 'react-router'
 
 import Navbar from '../../components/Navbar'
 import classes from './navbar.scss'
 
-export default class AdminLayoutNavbar extends Component {
+function mapStateToProps (state, props) {
+  const { session } = state
+  return {
+    username: session.getIn(['user', 'username'], ''),
+  }
+}
+
+export class AdminLayoutNavbar extends Component {
+  static propTypes = {
+    username: PropTypes.string.isRequired,
+  }
+
   render () {
+    const { username } = this.props
     return <Navbar>
       <div className={classes.content}>
         <Link className={classes.card} to='/'>
           <i className='icon icon-branches' />
         </Link>
         <div className={classes.user}>
-          管理员 FLOWYU
+          管理员 {username}
         </div>
       </div>
     </Navbar>
   }
 }
+
+export default connect(mapStateToProps)(AdminLayoutNavbar)
