5b8809c14668bb57835882651ab1805d858107cb
cy <cy@fir.im>
2017-11-28 16:45:51 +0800

    添加 isSuccesss、isSend 方法导出

--- src/packages/redux-http/status.js
+++ src/packages/redux-http/status.js
@@ -1,6 +1,16 @@
-export default {
+const enumStatus = {
   failure: -1,
   cancel: 0,
   send: 1,
   success: 2,
 }
+
+export function isSuccess (s) {
+  return s === enumStatus.success
+}
+
+export function isSend (s) {
+  return s === enumStatus.send
+}
+
+export default enumStatus
