b542f552332f00a898a71dff5a0a2dfad4aa70b5
cy <cy@fir.im>
2017-12-14 15:49:58 +0800

    添加 防抖

--- src/views/Admin/Plugins/List/toolbar.js
+++ src/views/Admin/Plugins/List/toolbar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+import debounce from 'util/debounce'
+
 import Input from 'rc-components/Input'
 import {
   TabBars,
@@ -19,19 +21,33 @@ export default class PluginToolBar extends Component {
       DISABLED: PropTypes.number,
       UPDATEABLED: PropTypes.number,
     }).isRequired,
+    delay: PropTypes.number,
+
     onChange: PropTypes.func.isRequired,
     onSearch: PropTypes.func.isRequired,
     i18n: PropTypes.func.isRequired,
+  }
+
+  static defaultProps = {
+    delay: 300,
   }
 
   state = {
     search: '',
   }
 
+  constructor (props, context) {
+    super(props, context)
+    const { delay } = this.props
+    this.doSearch = debounce((v) => {
+      const { onSearch } = this.props
+      onSearch(v)
+    }, delay)
+  }
+
   handleSearchChange = (v) => {
     this.setState({ search: v })
-    const { onSearch } = this.props
-    onSearch(v)
+    this.doSearch(v)
   }
 
   renderItem (category, counts) {
