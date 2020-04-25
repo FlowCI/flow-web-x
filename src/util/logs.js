e60e26f60d37ce07c11f75d1cb1179f30a3df17b
gy2006 <32008001@qq.com>
2020-04-26 01:04:23 +0200

    adapt logging format from agent

--- src/util/logs.js
+++ src/util/logs.js
@@ -1,27 +1,4 @@
-const decoder = new TextDecoder("utf-8")
-
-export class LogFromProto {
-
-  constructor (msg) {
-    this.msg = msg
-  }
-
-  // or step id
-  get cmdId () {
-    return this.msg.getCmdid()
-  }
-
-  get jobId () {
-    return this.msg.getJobid()
-  }
-
-  get log () {
-    // decode from uint8array
-    return decoder.decode(this.msg.getContent())
-  }
-}
-
-export class LogFromLoad {
+export class LogWrapper {
 
   constructor(cmdId, content) {
     this.id = cmdId
