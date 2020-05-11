3ad85732d323c754f02e77b07638c15aeea08919
gy2006 <32008001@qq.com>
2020-05-11 15:03:31 +0200

    add smtp secure type

--- src/view/Settings/Config/Smtp.vue
+++ src/view/Settings/Config/Smtp.vue
@@ -21,13 +21,14 @@
       ></text-box>
 
       <div>
-        <v-radio-group v-model="smtpOption.isSecure"
+        <v-radio-group v-model="smtpOption.secure"
                        row
                        dense
                        :rules="rules.required"
         >
-          <v-radio label="None" :value=false></v-radio>
-          <v-radio label="TLS/SSL" :value=true></v-radio>
+          <v-radio label="None" :value="SECURE_NONE"></v-radio>
+          <v-radio label="SSL" :value="SECURE_SSL"></v-radio>
+          <v-radio label="TLS" :value="SECURE_TLS"></v-radio>
         </v-radio-group>
       </div>
     </v-col>
@@ -37,6 +38,7 @@
 <script>
   import TextBox from '@/components/Common/TextBox'
   import { required } from '@/util/rules'
+  import { SECURE_NONE, SECURE_SSL, SECURE_TLS } from '@/util/configs'
 
   export default {
     name: "SettingsConfigSmtp",
@@ -51,6 +53,9 @@
     },
     data () {
       return {
+        SECURE_NONE,
+        SECURE_SSL,
+        SECURE_TLS,
         rules: {
           required: required('SMTP property is required')
         }
