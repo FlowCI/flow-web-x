b66c9974c482476367ad32b334547772c2c17454
gy2006 <32008001@qq.com>
2020-07-02 11:51:43 +0200

    apply btn on user and edit agent

--- src/view/Settings/Agent/EditAgent.vue
+++ src/view/Settings/Agent/EditAgent.vue
@@ -36,36 +36,19 @@
 
     <v-row>
       <v-col cols="8" class="text-end">
-        <v-dialog
-            v-model="dialog"
-            width="500"
-        >
-          <template v-slot:activator="{ on }">
-            <v-btn
-                outlined
-                color="error"
-                v-on="on"
-            >{{ $t('delete') }}
-            </v-btn>
+        <back-btn :onClick="onBackClick" class="mr-5"></back-btn>
+        <confirm-btn :text="$t('delete')"
+                     color="error"
+                     icon="mdi-delete"
+                     clazz="mr-5"
+                     @click="onDeleteClick">
+          <template v-slot:title>
+            <span class="red--text subheading">
+              Delete Agent {{ name }}?
+            </span>
           </template>
-          <v-card>
-            <v-card-title
-                class="error--text"
-                primary-title
-            >Delete Agent {{ name }}?
-            </v-card-title>
-
-            <v-divider></v-divider>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="primary" @click="dialog = false">{{ $t('cancel') }}</v-btn>
-              <v-btn outlined color="error" @click="onDeleteClick">{{ $t('delete') }}</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-btn outlined color="warning" @click="onBackClick" class="ml-4">{{ $t('back') }}</v-btn>
-        <v-btn color="primary" @click="onSaveClick" class="ml-4">{{ $t('save') }}</v-btn>
+        </confirm-btn>
+        <save-btn :onClick="onSaveClick"></save-btn>
       </v-col>
     </v-row>
   </div>
@@ -75,13 +58,19 @@
   import { mapState } from 'vuex'
   import actions from '@/store/actions'
   import TagEditor from '@/components/Common/TagEditor'
+  import ConfirmBtn from '@/components/Common/ConfirmBtn'
+  import SaveBtn from '@/components/Settings/SaveBtn'
+  import BackBtn from '@/components/Settings/BackBtn'
   import { AgentWrapper } from '@/util/agents'
   import { agentNameRules } from '@/util/rules'
 
   export default {
     name: 'SettingsAgentEdit',
     components: {
-      TagEditor
+      TagEditor,
+      ConfirmBtn,
+      SaveBtn,
+      BackBtn
     },
     data () {
       return {
