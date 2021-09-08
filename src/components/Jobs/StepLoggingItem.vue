5369f9ab67847ea56b8d1dac93a2b7820acfecb6
gy2006 <32008001@qq.com>
2021-09-08 21:21:59 +0200

    show download log btn when failure

--- src/components/Jobs/StepLoggingItem.vue
+++ src/components/Jobs/StepLoggingItem.vue
@@ -86,11 +86,11 @@
               </v-col>
 
               <v-col cols="1" class="caption" v-if="wrapper.isFinished && showLog">
-                <v-btn icon x-small @click="onLogDownload" v-if="wrapper.isSuccess">
+                <v-btn icon x-small @click="onLogDownload" v-if="wrapper.isSuccess || wrapper.isFailure">
                   <v-icon small>mdi-download</v-icon>
                 </v-btn>
 
-                <v-tooltip bottom v-else-if="wrapper.isFailure">
+                <v-tooltip bottom v-if="wrapper.isFailure">
                   <template v-slot:activator="{ on }">
                     <v-btn icon x-small @click="onRerunClick" v-on="on">
                       <v-icon small>mdi-restart</v-icon>
@@ -99,15 +99,17 @@
                   <div>{{ $t('job.hint.rerun_step') }}</div>
                 </v-tooltip>
 
-                <v-btn icon x-small v-else>
-                  <v-icon small>mdi-minus</v-icon>
-                </v-btn>
-
                 <span>
                   <v-icon class="ml-2" x-small>mdi-clock-outline</v-icon>
                   <span class="ml-1">{{ wrapper.duration }}</span>
                   <span class="ml-1">s</span>
                 </span>
+              </v-col>
+
+              <v-col cols="1" class="caption" v-else>
+                <v-btn icon x-small>
+                  <v-icon small>mdi-minus</v-icon>
+                </v-btn>
               </v-col>
             </v-row>
           </template>
