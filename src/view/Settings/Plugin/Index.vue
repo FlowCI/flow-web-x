280d9d9b697fc113c89dac8d85bd169ef5283336
gy2006 <32008001@qq.com>
2021-09-17 20:40:43 +0200

    init plugin setting page

---
+++ src/view/Settings/Plugin/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import actions from "@/store/actions";
+import {mapState} from "vuex";
+
+export default {
+  name: "PluginSettingsHome",
+
+  mounted() {
+    this.$emit('onConfigNav', {
+      navs: [{text: this.$t('settings.li.plugin')}],
+      showAddBtn: false
+    })
+
+    this.$store.dispatch(actions.plugins.list).then(() => {
+    })
+  },
+
+  computed: {
+    ...mapState({
+      plugins: state => state.plugins.items,
+    })
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
