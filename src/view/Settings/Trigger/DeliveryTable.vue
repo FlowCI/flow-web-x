a531ce5c36b7ebf66828d05645b777a7700d437e
gy2006 <32008001@qq.com>
2021-12-02 20:30:51 +0100

    add btn to show delivery desc

--- src/view/Settings/Trigger/DeliveryTable.vue
+++ src/view/Settings/Trigger/DeliveryTable.vue
@@ -1,21 +1,48 @@
 <template>
-  <v-data-table
-      :headers="[{text: 'Timestamp', value: 'timestamp'}, {text: 'Status', value: 'status'}, {text: 'Desc', value: 'desc'}]"
-      :items="delivery.items"
-      :options.sync="pagination"
-      :server-items-length="delivery.pagination.total"
-      :footer-props="{
+  <div>
+    <v-data-table
+        :headers="[{text: 'Timestamp', value: 'timestamp'}, {text: 'Status', value: 'status'}, {text: 'Desc', value: 'desc'}]"
+        :items="delivery.items"
+        :options.sync="pagination"
+        :server-items-length="delivery.pagination.total"
+        :footer-props="{
               itemsPerPageOptions: [10, 25, 50]
             }"
-  >
-    <template v-slot:item="{ item }">
-      <tr>
-        <td>{{ timeFormat(item.timestamp) }}</td>
-        <td>{{ item.status }}</td>
-        <td>{{ item.desc }}</td>
-      </tr>
-    </template>
-  </v-data-table>
+    >
+      <template v-slot:item="{ item }">
+        <tr>
+          <td>{{ timeFormat(item.timestamp) }}</td>
+          <td>
+            <v-icon small color="green" v-if="item.status === 'Success'">mdi-check-circle</v-icon>
+            <v-icon small color="red" v-else>mdi-close-circle-outline</v-icon>
+          </td>
+          <td>
+            <v-btn icon @click="onDetailDescClick(item.desc)" v-if="item.desc">
+              <v-icon small color="red">mdi-dots-horizontal</v-icon>
+            </v-btn>
+          </td>
+        </tr>
+      </template>
+    </v-data-table>
+
+    <v-dialog v-model="dialog" max-width="500">
+      <v-card>
+        <v-card-title></v-card-title>
+        <v-card-text>{{ dialogText }}</v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+              color="blue darken-1"
+              text
+              @click="dialog = false"
+          >
+            {{ $t('close') }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
@@ -38,6 +65,8 @@ export default {
   data() {
     return {
       timeFormat,
+      dialog: false,
+      dialogText: '',
       pagination: {
         page: 1,
         itemsPerPage: 10
@@ -67,6 +96,11 @@ export default {
       this.$store.dispatch(actions.triggers.deliveries, payload).then(() => {
         // console.log(this.obj)
       })
+    },
+
+    onDetailDescClick(text) {
+      this.dialogText = text
+      this.dialog = true
     }
   }
 }
