922bab87b68523f2e52c50fde87a76754d13dd00
gy2006 <32008001@qq.com>
2022-03-16 20:56:50 +0100

    support host on git config

--- src/view/Settings/Git/New.vue
+++ src/view/Settings/Git/New.vue
@@ -11,6 +11,13 @@
     </v-row>
 
     <v-row>
+      <v-col cols="7" v-if="selected.source === GIT_SOURCE_GERRIT">
+        <text-box label="Host"
+                  :rules="httpUrl"
+                  v-model="selected.host"
+        ></text-box>
+      </v-col>
+
       <v-col cols="7">
         <text-select :items="secretNameList"
                      label="Secret"
@@ -38,30 +45,46 @@
 import SaveBtn from '@/components/Settings/SaveBtn'
 import BackBtn from '@/components/Settings/BackBtn'
 import TextSelect from '@/components/Common/TextSelect'
-import {GitSourceSelection, GIT_SOURCE_GITHUB} from '@/util/git'
+import TextBox from '@/components/Common/TextBox'
+import {GIT_SOURCE_GERRIT, GIT_SOURCE_GITHUB, GitSourceSelection} from '@/util/git'
 import {mapState} from "vuex";
 import actions from "@/store/actions";
-import {CATEGORY_TOKEN} from "@/util/secrets";
+import {CATEGORY_AUTH, CATEGORY_TOKEN} from "@/util/secrets";
+import {httpUrl} from "@/util/rules"
+import code from "@/util/code";
 
 export default {
   name: "SettingsGitNew",
   components: {
     TextSelect,
+    TextBox,
     SaveBtn,
     BackBtn
   },
   data() {
     return {
       gitSourceList: GitSourceSelection,
+      GIT_SOURCE_GERRIT,
+      httpUrl: httpUrl("invalid url"),
       selected: {
         source: GIT_SOURCE_GITHUB,
-        secret: ''
+        secret: '',
+        host: '',
       },
       error: null
     }
   },
   mounted() {
-    this.loadRelatedSecret()
+
+  },
+  watch: {
+    selected: {
+      immediate: true,
+      deep: true,
+      handler: function (value) {
+        this.loadRelatedSecret()
+      }
+    }
   },
   computed: {
     ...mapState({
@@ -98,6 +121,11 @@ export default {
     loadRelatedSecret() {
       if (this.selected.source === GIT_SOURCE_GITHUB) {
         this.$store.dispatch(actions.secrets.listNameOnly, CATEGORY_TOKEN).then()
+        return
+      }
+
+      if (this.selected.source === GIT_SOURCE_GERRIT) {
+        this.$store.dispatch(actions.secrets.listNameOnly, CATEGORY_AUTH).then()
       }
     }
   }
